6b3608a5691d2d493c9c868a0148d37917501c27
Juan David Sanguino <1092526224@medellin.gov.co>
2023-11-20 22:54:17 -0500

    Add searchByModelAndFilter function to search controller and update search route

--- controllers/search.js
+++ controllers/search.js
@@ -20,4 +20,37 @@ const searchByFilter = async (req, res) => {
   });
 };
 
-export { searchByFilter };
+const searchByModelAndFilter = async (req, res) => {
+  const model = req.params.model;
+  const filter = req.params.filter;
+  const regex = new RegExp(filter, "i");
+
+  let data = [];
+
+  switch (model) {
+    case "users":
+      data = await User.find({ name: regex });
+      break;
+    case "hospitals":
+      data = await Hospital.find({ name: regex }).populate("user", "name");
+
+      break;
+    case "doctors":
+      data = await Doctor.find({ name: regex })
+        .populate("user", "name")
+        .populate("hospital", "name");
+      break;
+    default:
+      return res.status(400).json({
+        ok: false,
+        msg: "The available models are: users, hospitals and doctors",
+      });
+  }
+
+  res.json({
+    ok: true,
+    results: data,
+  });
+};
+
+export { searchByFilter, searchByModelAndFilter };
--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { validateJWT } from "../middleware/validate-jwt.js";
-import { searchByFilter } from "../controllers/search.js";
+import {
+  searchByFilter,
+  searchByModelAndFilter,
+} from "../controllers/search.js";
 
 const router = Router();
 
 router.get("/:filter", validateJWT, searchByFilter);
+router.get("/:model/:filter", validateJWT, searchByModelAndFilter);
 
 export default router;
